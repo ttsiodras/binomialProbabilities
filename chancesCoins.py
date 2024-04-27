30f4698aa2bffadd32f8d069860ffcd7535a37b2
Thanassis Tsiodras <ttsiodras@gmail.com>
2024-04-27 15:37:44 +0200

    Python3

--- chancesCoins.py
+++ chancesCoins.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import random, sys
 
 # How many times do you need to toss a coin to be 85% sure you'll see tails at least once? 
@@ -14,14 +14,14 @@ def choose(n,k):
 
 def experiment(ntimes):
     success = 0
-    for i in xrange(1, g_total):
-	for j in xrange(0, ntimes):
-	    if random.choice([0,1]) == 0:
-		success += 1
-		break
-    print "When trying", ntimes, "times, I got ", float(success)/g_total
+    for i in range(1, g_total):
+        for j in range(0, ntimes):
+            if random.choice([0,1]) == 0:
+                success += 1
+                break
+    print("When trying", ntimes, "times, I got ", float(success)/g_total)
 
 p = 0.5
-for n in xrange(2, 5):
+for n in range(2, 5):
     experiment(n)
-    print "The theory says...", sum([choose(n,i)*(p**i)*((1-p)**(n-i)) for i in xrange(1,n+1)])
+    print("The theory says...", sum([choose(n,i)*(p**i)*((1-p)**(n-i)) for i in range(1,n+1)]))
