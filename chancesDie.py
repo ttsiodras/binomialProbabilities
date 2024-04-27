d7fc731e4c7aea1e249ee3e4f8e758f37f2d015c
Thanassis Tsiodras <ttsiodras@gmail.com>
2024-04-27 15:46:02 +0200

    Python3

--- chancesDie.py
+++ chancesDie.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import random, sys
 
 # How many times do you need to toss a die to be 85% sure you'll see a one at least once? 
@@ -14,14 +14,14 @@ def choose(n,k):
 
 def experiment(ntimes):
     success = 0
-    for i in xrange(1, g_total):
-	for j in xrange(0, ntimes):
-	    if random.choice([1,2,3,4,5,6]) == 1:
-		success += 1
-		break
-    print "When trying", ntimes, "times, I got ", float(success)/g_total
+    for i in range(1, g_total):
+        for j in range(0, ntimes):
+            if random.choice([1,2,3,4,5,6]) == 1:
+                success += 1
+                break
+    print("When trying", ntimes, "times, I got ", float(success)/g_total)
 
 p = 1./6.
-for n in xrange(2, 15):
+for n in range(2, 15):
     experiment(n)
-    print "The theory says...", sum([choose(n,i)*(p**i)*((1-p)**(n-i)) for i in xrange(1,n+1)])
+    print("The theory says...", sum([choose(n,i)*(p**i)*((1-p)**(n-i)) for i in range(1,n+1)]))
